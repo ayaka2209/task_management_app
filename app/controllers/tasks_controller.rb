0c6d9741ceccb2433fae3110bd175e256ff0154a
ayaka tooya <na081543@icloud.com>
2022-10-27 15:32:33 +0900

    エラーメッセージ表記

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,29 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました!"
+    else
+      render :new
+    end
+  end
+
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
