a131f7d9b6af0de2bcf522a078f90f6048646487
ayaka tooya <na081543@icloud.com>
2022-10-28 16:50:35 +0900

    モデルテスト成功

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'rails_helper'
 RSpec.describe 'タスクモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
@@ -10,12 +9,14 @@ RSpec.describe 'タスクモデル機能', type: :model do
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
+        task = Task.new(title: '失敗テスト', content: '')
+        expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
+        task = Task.new(title: 'テスト', content: 'テスト')
+        expect(task).to be_valid
       end
     end
   end
