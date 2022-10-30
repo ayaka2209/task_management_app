c6f5f3dfd22e673fec2eee4debd71e56982b27b9
ayaka tooya <na081543@icloud.com>
2022-10-31 01:18:43 +0900

    taskテーブルにstatusカラム追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,16 +2,16 @@ FactoryBot.define do
 	factory :task do
 		title { '掃除' }
 		content { '換気扇の掃除' }
-		# created_at { '2022/10/29' }
+		expired_at { "2022/10/29" }
 	end
 	factory :second_task, class: Task do
 		title { 'お買い物' }
 		content { 'お弁当のおかず買う' }
-		# created_at { '2022/10/30' }
+		expired_at { "2022/10/30" }
 	end
 	factory :third_task, class: Task do
 		title { '病院に電話' }
 		content { '予防接種予約' }
-		# created_at { '2022/10/28' }
+		expired_at { "2022/10/28" }
 	end
 end
