040e72fb25cc077f6cbd4cef0657aeb0f93762b2
ayaka tooya <na081543@icloud.com>
2022-11-01 11:17:57 +0900

    bootstrap導入

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,15 +3,18 @@ FactoryBot.define do
 		title { '掃除' }
 		content { '換気扇の掃除' }
 		expired_at { "2022/10/29" }
+    status { "未着手" }
 	end
 	factory :second_task, class: Task do
 		title { 'お買い物' }
 		content { 'お弁当のおかず買う' }
 		expired_at { "2022/10/30" }
+    status { "着手中" }
 	end
 	factory :third_task, class: Task do
 		title { '病院に電話' }
 		content { '予防接種予約' }
 		expired_at { "2022/10/28" }
+    status { "完了" }
 	end
 end
