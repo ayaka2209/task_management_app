95f7719baa734c6f5c9c8ea5d7a11b4a24ee5cfd
ayaka tooya <na081543@icloud.com>
2022-11-06 17:30:56 +0900

    labelモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  has_many :labels, dependent: :destroy
 
   private
 
