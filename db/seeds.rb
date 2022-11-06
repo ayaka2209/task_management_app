231071ed7796cb2c893f35e18579f43ddbf58f51
ayaka tooya <na081543@icloud.com>
2022-11-06 14:58:21 +0900

    seedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(user_name: "テスト太郎",
-              email: "test@test.com",
+User.create!(user_name: "テスト",
+              email: "test@icloud.com",
               password: "password",
               admin: 'true'
             )
